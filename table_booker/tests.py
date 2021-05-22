fb57d850e55b2a54bb912d3f6ff1161f48d2d598
Collino <coadhola@gmail.com>
2021-05-22 22:03:11 +0100

    Testing Homepage

--- table_booker/tests.py
+++ table_booker/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from .factories import RestaurantFactory, TableFactory, UserFactory
+from .forms import BookingForm
+from .models import Restaurant
+
+
+class HomePageTests(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.restaurant = RestaurantFactory()
+
+    def test_authentication(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 302)
+
+    
+
+    def test_context_data(self):
+        self.client.force_login(self.user)
+        response = self.client.get("/")
+        context = response.context["restaurants"]
+
+        self.assertEqual(list(context), list(Restaurant.objects.all()))
+
+    def test_template_rendered(self):
+        self.client.force_login(self.user)
+        response = self.client.get("/")
+
+      
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")    
+    
+
+
